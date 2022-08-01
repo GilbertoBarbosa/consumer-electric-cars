493a4b29c051e0c0bd6b54cb94051db74b078b00
GilbertoBarbosa <gisantos555gmail.com>
2022-07-31 21:09:03 -0300

    Data exploration

--- script.R
+++ script.R
@@ -15,8 +15,14 @@ getwd()
 library('readxl')
 library('dplyr')
 library('caret')
+library('ggplot2')
+library('dplyr')
 
 set.seed(123)
+
+####################################################
+################## DATA EXPLORATION ################
+####################################################
 
 # Importing file
 df <- read_excel("FEV-data-Excel.xlsx")
@@ -27,20 +33,104 @@ count(df)
 # First rows
 head(df)
 
+# Finding null values
+sum(is.na(df))
+
+# Deleting rows that have null values
+df <- na.omit(df)
+
 # Statistics summary
 summary(df)
+str(df)
 
 # Renaming columns
 colnames(df) <-c('car', 'make', 'model', 'mprice', 'power', 'mtorque', 'tbrakes', 'dtype', 'bcapacity', 
                 'range', 'wheelbase', 'length', 'width','height','mweight', 'pweight', 'mcapacity', 
                 'nseats', 'ndoors','tsize', 'mspeed', 'bocapacity', 'acceleration', 'mcpower', 'cenergy')
-                
 
-# Deleting columns make, car and model
+# Categorical Variables
+
+# Make
+g <- ggplot(df, aes(make))
+g + geom_bar()
+
+# Type of brakes
+g <- ggplot(df, aes(tbrakes))
+g + geom_bar()
+
+# Drive type
+g <- ggplot(df, aes(dtype))
+g + geom_bar()
+
+
+# Numerical Variables
+
+# Minimal price ( * 100)
+df['mprice2'] = df['mprice']/100
+ggplot(df, aes(x=mprice2)) +
+  geom_boxplot()
+
+# Outlier Minimal Price:
+filter(df[c('car','mprice')], mprice == max(df['mprice']))
+
+# Engine power
+ggplot(df, aes(x=power)) +
+  geom_boxplot()
+
+# Outlier Engine power:
+filter(df[c('car','power')], power == max(df['power']))
+
+# Width
+ggplot(df, aes(x=width)) +
+  geom_boxplot()
+
+# Outlier Engine power:
+filter(df[c('car','width')], width == max(df['width']))
+
+# Maximum speed
+ggplot(df, aes(x=mspeed)) +
+  geom_boxplot()
+
+# Outlier Engine power:
+filter(df[c('car','mspeed')], mspeed == max(df['mspeed']))
+
+# Acceleration
+ggplot(df, aes(x=acceleration)) +
+  geom_boxplot()
+
+# Mean Energy consuption
+ggplot(df, aes(x=cenergy)) +
+  geom_boxplot()
+
+# Price x Acceleration
+ggplot(df, aes(x = mprice2, y = acceleration, colour = make)) + 
+  geom_point()
+
+# Price x Engine Power
+ggplot(df, aes(x = mprice2, y = power, colour = make)) + 
+  geom_point()
+
+# Energy consumption x Acceleration
+ggplot(df, aes(x = cenergy, y = acceleration, colour = make)) + 
+  geom_point() 
+
+# Energy consumption x Maximum Torque
+ggplot(df, aes(x = cenergy, y = mtorque, colour = make)) + 
+  geom_point() 
+
+# Energy Consumption x Maximum Empty Weight
+ggplot(df, aes(x = cenergy, y = mweight, colour = make)) + 
+  geom_point() 
+
+#####################################################
+################## FEATURE SELECTION ################
+#####################################################               
+
+# Deleting columns make, car and model because the algorithm
+# Linear Regression need only numeric values
 df$make <- NULL
 df$car <- NULL
 df$model <- NULL
-
 
 table(df$tbrakes)
 
@@ -56,11 +146,12 @@ df$dtype <- replace(df$dtype, df$dtype == "2WD (front)", 1)
 df$dtype <- replace(df$dtype, df$dtype == "2WD (rear)", 2)
 df$dtype <- replace(df$dtype, df$dtype == "4WD", 3)
 
-# Finding null values
-sum(is.na(df))
 
-# Deleting rows that have null values
-df <- na.omit(df)
+#####################################################
+################## MODEL TRAINING ##################
+####################################################
+
+# First Model
 
 # Dividing data in training and test
 split <- createDataPartition(y = df$cenergy, p = 0.7, list = FALSE)
@@ -68,7 +159,7 @@ split <- createDataPartition(y = df$cenergy, p = 0.7, list = FALSE)
 train_data <- df[split,]
 test_data <- df[-split,]
 
-# Training the first version of model with all columns
+# Training the first version of model with all variables
 model.v1 <- lm(cenergy ~ ., data = train_data)
 
 model.v1
@@ -76,11 +167,8 @@ model.v1
 # Summary first model
 summary(model.v1)
 
-predictedValues <- predict(model.v1, test_data)
-predictedValues
-plot(test_data$cenergy, predictedValues)
-
 # Second Model
+
 # In this model, only variables that had statistical significance
 # Pr(>|t|) less than 0.05 were considered.
 model.v2 <- lm(cenergy ~ dtype + range + bocapacity, data = train_data)
@@ -90,10 +178,11 @@ model.v2
 # Summary second model
 summary(model.v2)
 
+# Third Model
+
 # Deleting non-numeric variables
 df$tbrakes <- NULL
 df$dtype <- NULL
-
 
 # Correlation between all numeric variables
 cor(df[c('mprice', 'power', 'mtorque', 'bcapacity', 'range', 'wheelbase', 'length', 'width', 'height',
@@ -107,15 +196,17 @@ select.variables <- c('mprice', 'power', 'mtorque', 'bcapacity', 'wheelbase', 'l
                       'mweight', 'pweight', 'mcapacity', 'mspeed', 'bocapacity',
                       'mcpower', 'cenergy')
 
+# Correlation
 cor(df[select.variables])
 
 # Dividing data in training and test
 split <- createDataPartition(y = df$cenergy, p = 0.7, list = FALSE)
 
+# Training Third Model
 model.v3 <- lm(cenergy ~ mprice + power + mtorque + bcapacity + wheelbase
                + length + mweight + mcapacity + mspeed + bocapacity + mcpower, data = train_data)
 
 model.v3
 
-# Summary second model
+# Summary Third Model
 summary(model.v3)
